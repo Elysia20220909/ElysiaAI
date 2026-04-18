f012d39a85fda91e04a7bb66f028314767b9b9ec
shinsati1234 <shinsati1234@gmail.com>
2026-04-19 00:55:43 +0900

    feat: add cleanup and maintenance scripts alongside a standalone cheat sheet index page

--- packages/shield-agent/target/debug/deps/shield_agent-de1f8debbf25e669.d
+++
@@ -1,5 +0,0 @@
-C:\Users\hosih\OneDrive\ドキュメント\GitHub\ElysiaAI\packages\shield-agent\target\debug\deps\shield_agent-de1f8debbf25e669.d: src\main.rs
-
-C:\Users\hosih\OneDrive\ドキュメント\GitHub\ElysiaAI\packages\shield-agent\target\debug\deps\libshield_agent-de1f8debbf25e669.rmeta: src\main.rs
-
-src\main.rs:
