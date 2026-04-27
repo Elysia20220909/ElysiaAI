968d370d1ce51368faebc5eaa5f427a52103b130
Your Name <rutile47103973@gmail.com>
2026-04-27 23:26:23 +0900

    chore: remove project files and dependencies

--- dev/bare.ts
+++
@@ -1,5 +0,0 @@
-import { Elysia } from "elysia";
-
-new Elysia().get("/", () => "Hello Elysia").listen(3003);
-
-console.log("🦊 Elysia is running at http://localhost:3003");
--- dev/minimal.ts
+++
@@ -1,5 +0,0 @@
-import { Elysia } from "elysia";
-
-new Elysia().get("/", () => "OK").listen(3000);
-console.log("Server started on 3000");
-process.stdin.resume();
